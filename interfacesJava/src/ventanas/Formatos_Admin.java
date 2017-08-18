e55c626d36fc11be430b8a1dd64fd6516f53d698
“carlos” <crocha2@cuc.edu.co>
2017-08-18 09:18:33 -0500

    implementando jFrame de contratos

--- interfacesJava/src/ventanas/Formatos_Admin.java
+++ interfacesJava/src/ventanas/Formatos_Admin.java
@@ -236,9 +236,7 @@ public class Formatos_Admin extends javax.swing.JFrame {
 
     private void btnSalida1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalida1ActionPerformed
 
-        Contratos obj = new Contratos();
-        obj.setVisible(true);
-        dispose();
+       
         // TODO add your handling code here:
     }//GEN-LAST:event_btnSalida1ActionPerformed
 
